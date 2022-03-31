5c0dfbd68b95b6e5528db84f8e35431ed3226eab
Valerio Iorio <valexavi1234@gmail.com>
2022-03-31 15:59:48 +0200

    aggiunto house

---
+++ src/com/generation/housing/model/entities/House.java
@@ -0,0 +1,10 @@
+package com.generation.housing.model.entities;
+
+public class House 
+{
+    boolean isOnGraveyard;
+
+    Room r1 = new Room(5,6);
+    Room r2 = new Room(5,6);
+    Room r3 = new Room(5,6);
+}
